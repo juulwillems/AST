65f479d90c52736630a7dab34a0c5f453fd51fbc
Juul Willems <juulwillems92@gmail.com>
2014-04-09 17:49:56 +0200

    files are added

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ server.js
 var express = require('express');
 var app = express ();
 app.use(express.static(_dirname + '/public'));
-app listen (3000)
+app listen (process.env.PORT|| 3000})
 console.log("server is listening on port 3000")
