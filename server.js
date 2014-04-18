f05591026d6fec564abd0df2b52b3661d9076017
Juul Willems <juulwillems92@gmail.com>
2014-04-18 11:21:46 +0200

    no message

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express ();
-app.use(express.static(_dirname + '/public'));
-app listen (process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 3000);
 console.log("server is listening on port 3000")
