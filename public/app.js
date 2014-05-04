8e07ab5fe984ccf029d9c5c0190afef314c16537
Juul Willems <juulwillems92@gmail.com>
2014-05-04 13:33:09 +0200

    no message

--- public/app.js
+++ public/app.js
@@ -1,2 +1,19 @@
+var headings = $("h2");
+var paragraphs = $("p");
 
+
+$(function() {
+var headings = $("h2");
+var paragraphs = $("p");
+paragraphs.not(":first").hide();
+headings.on("click", function() {
+var t = $(this);
+var tPara = t.next();
+if(tPara.is(":visible")) {
+return;
+}
+paragraphs.slideUp("normal");
+tPara.slideDown("normal");
+});
+});
 
