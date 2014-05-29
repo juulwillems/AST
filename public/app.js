08b15e64f13e1f206ba909f0f652053176276994
Juul Willems <juulwillems92@gmail.com>
2014-05-29 11:26:14 +0200

    core collapse added

--- public/app.js
+++ public/app.js
@@ -1,16 +1,1 @@
 
- $(function() {
- var headings = $("h2");
- var paragraphs = $("p");
- paragraphs.not(":first").hide();
- headings.on("click", function() {
- var t = $(this);
- var tPara = t.next();
- if(tPara.is(":visible")) {
- return;
- }
- paragraphs.slideUp("normal");
- tPara.slideDown("normal");
- });
- });
-
